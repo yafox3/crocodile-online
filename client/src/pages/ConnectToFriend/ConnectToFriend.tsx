c21f651114a75b623b94a12adc34d8f1e7397868
Ruslan <ruslan.kret@yandex.ru>
2023-05-11 22:14:00 +0300

    Сделал страницу подключения к комнате друга

--- client/src/pages/ConnectToFriend/ConnectToFriend.tsx
+++ client/src/pages/ConnectToFriend/ConnectToFriend.tsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import s from './ConnectToFriend.module.scss'
+import Logo from '../../components/Logo/Logo'
+import ControlBox from '../../components/ControlBox/ControlBox'
+import Input from '../../components/UI/Input/Input'
+import Button from '../../components/UI/Button/Button'
 
 const ConnectToFriend: React.FC = () => {
-	return <div>ConnectToFriend</div>
+	return (
+		<>
+			<Logo />
+			<ControlBox>
+				<h2 className='title'>Введите ID комнаты</h2>
+				<div className={s.form}>
+					<Input
+						placeholder='ID комнаты'
+						w='100%'
+						h='50px'
+					/>
+					<Button
+						variant='primary' 
+						w='190px'
+						h='40px'
+					>
+						Подключиться
+					</Button>
+					<Button
+						variant='secondary' 
+						w='190px'
+						h='40px'
+					>
+						Назад
+					</Button>
+				</div>
+			</ControlBox>
+		</>
+	)
 }
 
 export default ConnectToFriend
