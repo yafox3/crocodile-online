54d0b78f39e6f671c79a3b1da6e0d7c8ab9ff111
Ruslan <ruslan.kret@yandex.ru>
2023-05-13 14:32:17 +0300

    добавил обработку сообщений

--- server/index.js
+++ server/index.js
@@ -7,30 +7,33 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 
 app.ws('/', (ws, req) => {
-    ws.on('message', (msg) => {
-        msg = JSON.parse(msg)
-        switch (msg.method) {
-            case "connection":
-                connectionHandler(ws, msg)
-                break
-            case "draw":
-                broadcastConnection(ws, msg)
-                break
-        }
-    })
+	ws.on('message', msg => {
+		msg = JSON.parse(msg)
+		switch (msg.method) {
+			case 'connection':
+				connectionHandler(ws, msg)
+				break
+			case 'draw':
+				broadcastConnection(ws, msg)
+				break
+			case 'sendMessage':
+				broadcastConnection(ws, msg)
+				break
+		}
+	})
 })
 
 app.listen(PORT, () => console.log('working...'))
 
 const connectionHandler = (ws, msg) => {
-    ws.id = msg.id
-    broadcastConnection(ws, msg)
+	ws.id = msg.id
+	broadcastConnection(ws, msg)
 }
 
 const broadcastConnection = (ws, msg) => {
-    aWss.clients.forEach(client => {
-        if (client.id === msg.id) {
-            client.send(JSON.stringify(msg))
-        }
-    })
+	aWss.clients.forEach(client => {
+		if (client.id === msg.id) {
+			client.send(JSON.stringify(msg))
+		}
+	})
 }
